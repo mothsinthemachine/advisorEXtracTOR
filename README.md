891ff0a1b504926951fe3cff6f4261c0acf2c76e
mothsinthemachine <sherwoodj@cochise.edu>
2024-03-28 14:54:53 -0700

    Renaming

--- README.md
+++ README.md
@@ -1,10 +1,74 @@
-# AdvisorTrac Report Extractor
-`> at-report-extract.py <source> [destination]`
+# advisorEXtracTOR
 
-Extraction tool for AdvisorTrac Reports (typically named `reportOut.4sp`).
+AdvisorTrac Report Extractor (version 3)
 
-## Benchmarks
+Author: Jake Sherwood ([GitHub Profile](https://github.com/mothsinthemachine))
 
-[x] Successfully extract data from AdvisorTrac generated report.\
-[x] Extract data from AdvisorTrac reports in folder.
+
+## Purpose
+
+The purpose of this program is to take report text data from AdvisorTrac and 
+convert it into a CSV file for use with Excel.
+
+Values from these reports are delivered by either HTML (screen) or TEXT (.4sp). 
+However, to use these values in Excel requires a trained data-entry eye and 
+deep attention, making it hard to import the data. It requires extreme Excel 
+know-how or manual entry.
+
+I wrote this code to help me with this and make data reporting from AdvisorTrac 
+easier. It's written for a specific department in a specific institution, so it 
+may not work for all.
+
+
+## Requires
+
+- Python3 installed
+
+	
+## Process
+
+1.	Get the data from AdvisorTrac delivered in TEXT (.4sp) form and save it to 
+	the computer. Remember the location of this file.
+
+2.	Open a command line interface. Suggestions are:
+
+	- Command Prompt (Windows)
+	- Powershell (Windows)
+	- Terminal (Windows or *nix, Mac included)
+
+3.	Use Python to run the script.
+
+	Possible Syntax:
+
+		python at-report-extract.py <source (required)> <destination (optional)>
+
+	or
+
+		python3 at-report-extract.py <source (required)> <destination (optional)>
+
+4.	Profit.
+
+
+## Revisions
+
+03/27/24 - Renamed repo from 'at-report-extract to' to 'advisorEXtracTOR' because
+			it sounds more amazing.
+
+02/14/24 - Separating tasks into new functions. Formatting output file. Now can
+			input multiple files in a directory.
+
+01/24/24 - New version completed. Renamed old version to 'at-report-extract-v3.py'
+
+01/23/24 - Bug discovered in version 2 code. Needed to overhaul file-writing
+			design.
+
+01/17/24 - New version completed. Renamed old version to 'at-report-extract-v1.py.'
+
+01/16/24 - Created new version of tool. WIP.
+
+01/10/24 - Renamed directories as A and B (as in 'how to get from A to B'). 
+			Outputs by default to B directory as 'output.csv'.
+
+01/09/24 - Initial script and commit to GitHub private repo
+
 
--- advisorEXtracTOR.py
+++ advisorEXtracTOR.py
@@ -1,70 +1,37 @@
 '''
-AdvisorTrac Report Extractor (version 3)
-at-report-extract.py
+	advisorEXtracTOR
+	AdvisorTrac Report Extractor (version 3)
 
-Author: Jake Sherwood (MothsInTheMachine on GitHub)
-Last Modified: Jan. 24, 2024
-
-Purpose:
-The purpose of this program is to take report text data from AdvisorTrac and 
-convert it into a CSV file for use with Excel.
-
-Reason:
-Values from these reports are delivered by either HTML (screen) or TEXT (.4sp). 
-However, to use these values in Excel requires a trained data-entry eye and 
-deep attention, making it hard to import the data. It requires extreme Excel 
-know-how or manual entry.
-
-I wrote this code to help me with this and make data reporting from AdvisorTrac 
-easier. It's written for a specific department in a specific institution, so it 
-may not work for all.
-
-Requires:
-	- Python3 installed
-
-Process:
-1.	Get the data from AdvisorTrac delivered in TEXT (.4sp) form and save it to 
-	the computer. Remember the location of this file.
-2.	Open a command line interface. Suggestions are:
-	- Command Prompt (Windows)
-	- Powershell (Windows)
-	- Terminal (Windows or *nix, Mac included)
-3.	Use Python to run the script.
-
-	Possible Syntax:
-
-	> python at-report-extract.py <source (required)> <destination (optional)>
-
-		or
-
-	> python3 at-report-extract.py <source (required)> <destination (optional)>
-
-4.	Profit.
-
-Revisions:
-
-02/14/24 - Separating tasks into new functions. Formatting output file. Now can
-			input multiple files in a directory.
-
-01/24/24 - New version completed. Renamed old version to 'at-report-extract-v3.py'
-
-01/23/24 - Bug discovered in version 2 code. Needed to overhaul file-writing
-			design.
-
-01/17/24 - New version completed. Renamed old version to 'at-report-extract-v1.py.'
-
-01/16/24 - Created new version of tool. WIP.
-
-01/10/24 - Renamed directories as A and B (as in 'how to get from A to B'). 
-			Outputs by default to B directory as 'output.csv'.
-
-01/09/24 - Initial script and commit to GitHub private repo
+	Author: Jake Sherwood (MothsInTheMachine on GitHub)
+	Last Modified: Mar. 27, 2024
 '''
 
 import sys
 import os, os.path
 from datetime import datetime
 
+
+# Define MAIN
+def main():
+	labLabels = ['DC Math Lab','DC Writing Lab','SV Math Lab','SV Writing Lab']
+	keywords = defineKeywords(labLabels)
+	inputPath,outputPath = verifyArguments(len(sys.argv))
+
+	printHeader()
+	
+	if not (verifyOverwrite(outputPath)):
+		print("Cancelling process. Please run command again later.")
+		printFooter()
+		exit()
+
+	
+	processFiles(inputPath, outputPath, keywords, labLabels, [])
+	printFooter()
+	exit()
+# End MAIN
+
+
+# FUNCTION DEFINITIONS
 
 def printHeader():
 	print()
@@ -157,12 +124,6 @@ def verifyOverwrite(outputPath):
 	else:
 		# Path does not exists
 		return True
-
-
-	# if not (os.path.exists(inputPath)):
-	# 	quitProcess(f'the input file path \"{inputPath}\" does not exist')
-	# else:
-	# 	print(f'Extractor will process file found at \"{inputPath}\" and output to \"{outputPath}\".')
 	
 
 def processFiles(inputPath,outputPath,keywords,labLabels,totals):
@@ -216,7 +177,6 @@ def processFiles(inputPath,outputPath,keywords,labLabels,totals):
 		# end of for-loop
 	
 	# Write to file
-	
 	for element in totals:
 		if element[0] in labLabels:
 			outputFile.write('\n\n' + sep.join([element[0],'Visits','Hours','Students']))
@@ -236,24 +196,6 @@ def processFiles(inputPath,outputPath,keywords,labLabels,totals):
 	outputFile.close()
 	return True
 
-# MAIN function
-def main():
-	labLabels = ['DC Math Lab','DC Writing Lab','SV Math Lab','SV Writing Lab']
-	keywords = defineKeywords(labLabels)
-	inputPath,outputPath = verifyArguments(len(sys.argv))
 
-	printHeader()
-	
-	if not (verifyOverwrite(outputPath)):
-		print("Cancelling process. Please run command again later.")
-		printFooter()
-		exit()
-
-	
-	processFiles(inputPath, outputPath, keywords, labLabels, [])
-	
-	printFooter()
-	exit()
-
-# Run the script's MAIN function
+# Call MAIN
 main()
